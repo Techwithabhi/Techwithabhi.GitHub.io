dc0d0971a56446979f762cc90bef0fdde5b48c89
Abhi Sarkar <abhisarkar6038@gmail.com>
2026-02-12 11:41:59 +0530

    Add js file of contact code

---
+++ assets/js_file/mail.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    emailjs.init("Cf_Y3oLLUVj_A1FwR"); // Public Key
+
+    const form = document.getElementById("contact-form");
+
+    if (!form) {
+        console.error("Contact form not found!");
+        return;
+    }
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            "service_jt9kk8j",
+            "template_z2jwjvr",
+            this
+        ).then(
+            function () {
+                alert("Message sent successfully 🚀");
+                form.reset();
+            },
+            function (error) {
+            console.error("EmailJS Error FULL:", error);
+            alert("Failed: " + error.text);
+            }
+        );
+    });
+
+});
